9183de6da905ff2d6453406a89346a1fa4ff9e2c
Myralllka <Olli250@ukr.net>
2019-02-13 23:57:47 +0200

    13.02.2019 v1

---
+++ twitter1.py
@@ -0,0 +1,73 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+from docs import twurl
+import ssl
+import json
+import sys
+
+
+# https://apps.twitter.com/
+# Create App and get the four strings, put them in hidden.py
+
+
+def input_account():
+    """
+    :return: account_name if it is not empty, else exit from the application
+    """
+    account_name = input('Enter Twitter Account:')
+    if account_name:
+        return account_name
+    sys.exit()
+
+
+def initial():
+    """
+
+    """
+    account_name = input_account()
+    url = twurl.augment(TWITTER_URL, {'screen_name': account_name})
+    connection = urllib.request.urlopen(url, context=ctx)
+    headers = dict(connection.getheaders())
+    print('Remaining', headers['x-rate-limit-remaining'])
+    data = connection.read().decode()
+    with open('./tmp/data.json', 'w') as out_file:
+        print(data, file=out_file)
+
+
+def main_json_travel():
+    """
+
+    :return: 0 if program finished without errors, -1 if any error occurred
+    """
+    with open('./tmp/data.json', encoding='utf-8') as ff:
+        data = json.load(fp=ff)
+    current = data
+    while True:
+        try:
+            if type(current) == dict:
+                print(' | '.join(list(i for i in current)))
+                current = eval('current' + '[\'' + input('direction: ') + '\']')
+            elif type(current) == list:
+                print(' | '.join(list(i['name'] for i in current)))
+                path, c = input('direction: '), 0
+                for i in range(len(current)):
+                    if current[i]['name'] == path:
+                        c = i
+                        break
+                current = eval('current' + '[' + str(c) + ']')
+            else:
+                print(current)
+                return 0
+        except KeyError:
+            return -1
+
+
+if __name__ == "__main__":
+    # Ignore SSL certificate errors
+    TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    initial()
+    main_json_travel()
